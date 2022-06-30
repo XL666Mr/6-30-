f7da091d930eec40a3666e2ec5b5e76eea90f1df
XL_Mr <643158351@qq.com>
2022-06-30 22:41:51 +0800

    彻底完成

--- src/App.vue
+++ src/App.vue
@@ -1,121 +1,102 @@
 <template>
   <div id="app">
-    <div class="container">
-      <!-- 顶部框模块 -->
-      <div class="form-group">
-        <div class="input-group">
-          <h4>品牌管理</h4>
-        </div>
-      </div>
-
-      <!-- 数据表格 -->
-      <table class="table table-bordered table-hover mt-2">
-        <thead>
-          <tr>
-            <th>编号</th>
-            <th>资产名称</th>
-            <th>价格</th>
-            <th>创建时间</th>
-            <th>操作</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(item,index) in list" :key="item.id">
-            <td>{{ item.id }}</td>
-            <td>{{ item.name }}</td>
-
-            <!-- 如果价格超过100，就有red这个类 -->
-            <td :class="{ red: item.price > 100 }">{{ item.price }}</td>
-            <td>{{ item.time }}</td>
-            <td><a href="#" @click="del(index)">删除</a></td>
-          </tr>
-          <!-- <tr style="background-color: #EEE">
-              <td>统计:</td>
-              <td colspan="2">总价钱为: 0</td>
-              <td colspan="2">平均价: 0</td>
-          </tr> -->
-        </tbody>
-        <!-- 
-        <tfoot >
-          <tr>
-            <td colspan="5" style="text-align: center">暂无数据</td>
-          </tr>
-        </tfoot>
-            -->
+    <div>
+      <span>姓名:</span>
+      <input type="text" v-model.trim="name" />
+    </div>
+    <div>
+      <span>年龄:</span>
+      <input type="number" v-model.trim.number="age" />
+    </div>
+    <div>
+      <span>性别:</span>
+      <select v-model="sex">
+        <option value="男">男</option>
+        <option value="女">女</option>
+      </select>
+    </div>
+    <div>
+      <button @click="add()">添加/修改</button>
+    </div>
+    <div>
+      <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+          <th>序号</th>
+          <th>姓名</th>
+          <th>年龄</th>
+          <th>性别</th>
+          <th>操作</th>
+        </tr>
+        <tr v-for="(item, index) in list" :key="item.id">
+          <td>{{ item.id }}</td>
+          <td>{{ item.name }}</td>
+          <td>{{ item.age }}</td>
+          <td>{{ item.sex }}</td>
+          <td>
+            <button @click="del(index)">删除</button>
+            <button @click="change(index)">编辑</button>
+          </td>
+        </tr>
       </table>
-
-      <!-- 添加资产 -->
-      <form class="form-inline" style="display: flex">
-        <div class="form-group">
-          <div class="input-group">
-            <input
-              type="text"
-              class="form-control"
-              placeholder="资产名称"
-              v-model="name"
-            />
-          </div>
-        </div>
-        &nbsp;&nbsp;&nbsp;&nbsp;
-        <div class="form-group">
-          <div class="input-group">
-            <input
-              type="text"
-              class="form-control"
-              placeholder="价格"
-              v-model="price"
-            />
-          </div>
-        </div>
-        &nbsp;&nbsp;&nbsp;&nbsp;
-        <!-- 阻止表单提交 -->
-        <button class="btn btn-primary" @click.prevent="btn">添加资产</button>
-      </form>
     </div>
   </div>
 </template>
-
 <script>
-// 1. 明确需求
-// 2. 标签+样式+默认数据
-// 3. 下载bootstrap, main.js引入bootstrap.css
-// 4. 把list数组 - 铺设表格
-// 5. 修改价格颜色
 export default {
   data() {
     return {
-      list: [
-        { id: 100, name: "外套", price: 199, time: new Date("2010-08-12") },
-        { id: 101, name: "裤子", price: 34, time: new Date("2013-09-01") },
-        { id: 102, name: "鞋", price: 25.4, time: new Date("2018-11-22") },
-        { id: 103, name: "头发", price: 19900, time: new Date("2020-12-12") },
-      ],
+      list: [],
       name: "",
-      price: 0,
+      age: "",
+      sex: "男",
+      flage: false,
+      num: -1,
     };
   },
   methods: {
-    btn() {
-      if(this.name == '' || this.price == 0) return alert('请输入完整的内容')
-      const id = this.list[this.list.length - 1]  ? this.list[this.list.length - 1].id + 1 : 100;
-      this.list.push({
-        id,
-        name: this.name,
-        price: this.price,
-        time: new Date(),
-      });
-      this.name = '',
-      this.price = 0
+    add() {
+        if(this.name =='' || this.age =='') return alert('请输入完整的内容')
+      if (this.flage) {
+        this.list[this.num].name = this.name
+        this.list[this.num].age = this.age 
+        this.flage = false;
+      } else {
+        const id = this.list[this.list.length - 1]
+          ? this.list[this.list.length - 1].id + 1
+          : 0;
+        this.list.push({
+          id,
+          name: this.name,
+          age: this.age,
+          sex: this.sex,
+        }),
+          (this.name = "");
+        this.age = "";
+        this.sex = "男";
+      }
     },
-    del(i){
-      this.list.splice(i,1)
-    }
+    del(i) {
+      this.list.splice(i, 1);
+      this.list.forEach((ele, index) => {
+        ele.id = index;
+      });
+    },
+    change(i) {
+      (this.name = this.list[i].name),
+        (this.age = this.list[i].age),
+        (this.sex = this.list[i].sex);
+      this.flage = true;
+      this.num = i;
+    },
   },
 };
 </script>
-
-<style >
-.red {
-  color: red;
+<style>
+table {
+  border-collapse: collapse;
+}
+th,
+td {
+  border: 1px solid #000;
 }
 </style>
